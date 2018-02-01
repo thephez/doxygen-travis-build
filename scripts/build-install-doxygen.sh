5088b90c3115cf3b76811f2821d2f6c8d507e763
thephez <thephez@users.noreply.github.com>
2018-02-01 12:48:14 -0500

    Add Doxygen build script

---
+++ scripts/build-install-doxygen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+# Build instructions from: https://www.stack.nl/~dimitri/doxygen/download.html
+echo "Cloning doxygen repository..."
+git clone https://github.com/doxygen/doxygen.git
+du -h
+
+cd doxygen
+pwd
+
+echo "Create build folder..."
+mkdir build
+cd build
+
+echo "Make..."
+cmake -G "Unix Makefiles" ..
+make
+
+echo "Done building doxying..."
+du -h
+
+#echo "Install Doxygen"
+#sudo make install
