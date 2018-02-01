a5ff53757086cf1b22ea9685a862d37073b17428
thephez <thephez@users.noreply.github.com>
2018-02-01 14:27:28 -0500

    debug - Figure out proper foxygen path

--- scripts/generateDocumentationAndDeploy.sh
+++ scripts/generateDocumentationAndDeploy.sh
@@ -50,7 +50,9 @@ cp $TRAVIS_BUILD_DIR/doxygen/* doc/
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-.$TRAVIS_BUILD_DIR/doxygen $DOXYFILE 2>&1 | tee doxygen.log
+echo .$TRAVIS_BUILD_DIR/doxygen
+echo "doxyfile: " doc/$DOXYFILE
+.$TRAVIS_BUILD_DIR/doxygen doc/$DOXYFILE 2>&1 | tee doxygen.log
 
 echo 'Done generating Doxygen code documentation...'
 #cat doxygen.log
