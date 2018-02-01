c1c645d9a316524b96f940580c5dc2d18f870b67
thephez <thephez@users.noreply.github.com>
2018-02-01 14:09:00 -0500

    script - Doxygen generate update

--- scripts/generateDocumentationAndDeploy.sh
+++ scripts/generateDocumentationAndDeploy.sh
@@ -50,7 +50,7 @@ cp $TRAVIS_BUILD_DIR/doxygen/* doc/
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-./$TRAVIS_BUILD_DIR/doxygen $DOXYFILE 2>&1 | tee doxygen.log
+.$TRAVIS_BUILD_DIR/doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
 echo 'Done generating Doxygen code documentation...'
 #cat doxygen.log
