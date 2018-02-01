576c74dad537aa190f86ad1a425cc60e5c943e93
thephez <thephez@users.noreply.github.com>
2018-02-01 14:04:32 -0500

    scripts - Doxygen updates  - Build doxygen 1.18.14  - Copy doxygen bin to more convenient location and call properly

--- scripts/build-install-doxygen.sh
+++ scripts/build-install-doxygen.sh
@@ -8,6 +8,9 @@ git clone https://github.com/doxygen/doxygen.git
 
 cd doxygen
 
+# Use v1.8.14 (2f4139de014bf03898320a45fe52c92872c1e0f4)
+git checkout 2f4139de014bf03898320a45fe52c92872c1e0f4 .
+
 echo "Create build folder..."
 mkdir build
 cd build
@@ -20,6 +23,7 @@ echo "Done building doxygen..."
 ./bin/doxygen
 
 echo "Install Doxygen"
+cp ./bin/* $TRAVIS_BUILD_DIR
 make install
 
 cd $TRAVIS_BUILD_DIR
